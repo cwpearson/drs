9c4710498b6cc8c2369a2b851706255ffa8b51a6
Carl Pearson <pearson@illinois.edu>
2017-05-31 05:26:20 -0700

    .

--- prototypes/vecadd.py
+++ prototypes/vecadd.py
@@ -1,3 +1,6 @@
+import mmap
+import struct
+
 class DrsVectorDouble:
     def __init__(self, reg, off, cap):
         self.region = reg
@@ -5,4 +8,26 @@ class DrsVectorDouble:
         self.capacity = cap
 
     def __getitem__(self, key):
-        return 0.0
+        base = self.region + self.offset
+        return struct.unpack('d', base[0:8])
+
+    def __setitem__(self, key, item):
+        d = struct.pack('d', item)
+
+
+
+with open("foo", "wr+b") as f:
+    # memory-map the file, size 0 means whole file
+    mm = mmap.mmap(f.fileno(), 0)
+    # read content via standard file methods
+    print mm.readline()  # prints "Hello Python!"
+    # read content via slice notation
+    print mm[:5]  # prints "Hello"
+    # update content using slice notation;
+    # note that new content must have same size
+    mm[6:] = " world!\n"
+    # ... and read again using standard file methods
+    mm.seek(0)
+    print mm.readline()  # prints "Hello  world!"
+    # close the map
+    mm.close()
