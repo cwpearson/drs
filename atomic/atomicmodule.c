e8ef49e68754be09ae2e8227fb923ab753ae4f45
Carl Pearson <pearson@illinois.edu>
2017-05-31 12:35:06 -0700

    new function

--- atomic/atomicmodule.c
+++ atomic/atomicmodule.c
@@ -1,22 +1,31 @@
 #include <Python.h>
 
+#include <stdint.h>
+#include <stdatomic.h>
 
-// The function that will be called for atomic.add_double()
+// The function that will be called for atomic.add_int()
+// should take a numpy array, and offset, and an increment
 static PyObject *
-atomic_add_double(PyObject *self, PyObject *args)
+atomic_add_int(PyObject *self, PyObject *args)
 {
-    double d;
-    int sts;
+    int32_t off;
+    int32_t inc;
+    PyObject *np_array;
 
-    if (!PyArg_ParseTuple(args, "f", &d))
+    if (!PyArg_ParseTuple(args, "Oii", &np_array, &off, &inc))
         return NULL;
-    sts = system(command);
-    return Py_BuildValue("i", sts);
+
+    fprintf(stderr, "off=%d, inc=%d\n", off, inc);
+    //fprintf(stderr, "%p\n", objptr);
+
+    // Treat the object like a raw int and increment it
+    //atomic_fetch_add_explicit((atomic_int*)objptr, inc, memory_order_relaxed);    
+    return Py_BuildValue("i", inc);
 }
 
 static PyMethodDef AtomicMethods[] = {
-    {"add_double",  atomic_add_double, METH_VARARGS,
-     "Atomic add on an array"},
+    {"add_int",  atomic_add_int, METH_VARARGS,
+     "Atomic integer addition"},
     {NULL, NULL, 0, NULL}        /* Sentinel */
 };
 
