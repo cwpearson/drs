c97624540ff76c22d36a109d644cf20bb6263f1d
Carl Pearson <pearson@illinois.edu>
2017-06-01 04:35:04 -0700

    simplify

--- atomic/atomicmodule.c
+++ atomic/atomicmodule.c
@@ -18,45 +18,51 @@ atomic_add_int32(PyObject *self, PyObject *args)
     fprintf(stderr, "off=%d, inc=%d\n", off, inc);
     fprintf(stderr, "passed obj: %p\n", np_array);
 
-    // Get a pointer to the array data
-    // see https://docs.scipy.org/doc/numpy/reference/arrays.interface.html
-    PyObject *array_interface = PyObject_GetAttrString(np_array, "__array_interface__");
-    if (NULL == array_interface) {
-      PyErr_SetString(PyExc_AttributeError, 
-        "Expecting object in argument 0 to have an __array_interface__ attribute");
-      return NULL;
-    }
     PyObject *ctypes = PyObject_GetAttrString(np_array, "ctypes");
     if (NULL == ctypes) {
       PyErr_SetString(PyExc_AttributeError, 
-        "Expecting object in argument 0 to have a ctypes attribute");
+        "Expecting argument 0 to have a ctypes attribute");
+      return NULL;
+    }
+    PyObject *data = PyObject_GetAttrString(ctypes, "data");
+    if (NULL == data) {
+      PyErr_SetString(PyExc_AttributeError, 
+        "Expecting argument 0 to have a ctypes.data attribute");
       return NULL;
     }
 
-    PyObject *dataStr = PyString_FromString("data");
-    PyObject *tup = PyObject_GetItem(array_interface, dataStr);
-    if (NULL == tup) {
-      PyErr_SetString(PyExc_LookupError, 
-        "Argument 0 should provide __array_interface__['data']");
-      return NULL;
-    }
-    if (!PyTuple_Check(tup)) {
-      PyErr_SetString(PyExc_TypeError, "__array_interface__['data'] should be a tuple");
-      return NULL;
-    }
+    // Get a pointer to the array data
+    // see https://docs.scipy.org/doc/numpy/reference/arrays.interface.html
+    //PyObject *array_interface = PyObject_GetAttrString(np_array, "__array_interface__");
+    //if (NULL == array_interface) {
+    //  PyErr_SetString(PyExc_AttributeError, 
+    //    "Expecting object in argument 0 to have an __array_interface__ attribute");
+    //  return NULL;
+    //}
+    // PyObject *dataStr = PyString_FromString("data");
+    // PyObject *tup = PyObject_GetItem(array_interface, dataStr);
+    // if (NULL == tup) {
+    //   PyErr_SetString(PyExc_LookupError, 
+    //     "Argument 0 should provide __array_interface__['data']");
+    //   return NULL;
+    // }
+    // if (!PyTuple_Check(tup)) {
+    //   PyErr_SetString(PyExc_TypeError, "__array_interface__['data'] should be a tuple");
+    //   return NULL;
+    // }
 
-    PyObject *pointer_as_int = PyTuple_GetItem(tup, 0);
-    if (!PyInt_Check(pointer_as_int)) {
-      PyErr_SetString(PyExc_TypeError, "0th tuple item should be an integer");
-      return NULL;
-    }
+    // PyObject *pointer_as_int = PyTuple_GetItem(tup, 0);
+    // if (!PyInt_Check(pointer_as_int)) {
+    //   PyErr_SetString(PyExc_TypeError, "0th tuple item should be an integer");
+    //   return NULL;
+    // }
 
     atomic_int *ptr = NULL;
-    if (PyInt_CheckExact(pointer_as_int)) {
-      long l = PyInt_AsLong(pointer_as_int);
+    if (PyInt_CheckExact(data)) {
+      long l = PyInt_AsLong(data);
       ptr = (void *) l; 
-    } else if (PyLong_CheckExact(pointer_as_int)) {
-      ptr = PyLong_AsVoidPtr(pointer_as_int);
+    } else if (PyLong_CheckExact(data)) {
+      ptr = PyLong_AsVoidPtr(data);
     } else {
       assert(0 && "Unhandled.");
     }
